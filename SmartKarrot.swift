e8223d4cc79617b52be2b14d90ca01d984334453
Pranay2019 <57348828+Pranay2019@users.noreply.github.com>
2019-11-11 11:08:42 +0530

    Add files via upload

---
+++ SmartKarrot.swift
@@ -0,0 +1,40 @@
+//
+//  SmartKarrot.swift
+//  Sample
+//
+//  Created by Pranay Kumar on 10/31/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
+import UsageAnalytics
+
+@objc(SmartKarrot)
+class SmartKarrot: NSObject {
+  
+  //Usage Analytics
+  @objc func setAppId(_ appId: String) {
+      UsageAnalytics.shared.configure(appId: appId)
+  }
+  
+  @objc func setUserId(_ userId: String) {
+    UsageAnalytics.shared.set(userId: userId)
+  }
+  
+  @objc func resetUserId() {
+    UsageAnalytics.shared.resetUserId()
+  }
+  
+  @objc func setUserAttribute(_ value: Any, key: String) {
+    UsageAnalytics.shared.set(value: value, for: key)
+  }
+  
+  //Log Events
+  @objc func logEvent(_ name: String) {
+    UsageAnalytics.shared.log(event: name)
+  }
+  
+  @objc func logEventWithParameters(_ value: String, key: [String: Any]) {
+      UsageAnalytics.shared.log(event: value, with: key)
+  }
+}
